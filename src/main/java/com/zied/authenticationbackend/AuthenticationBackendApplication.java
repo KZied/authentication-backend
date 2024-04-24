f6e4f0aaa9794f2d64e9dfe41164441e6d40c578
Zied <zied.kannou15@gmail.com>
2024-04-24 21:01:59 +0200

    User entity created

--- src/main/java/com/zied/authenticationbackend/AuthenticationBackendApplication.java
+++ src/main/java/com/zied/authenticationbackend/AuthenticationBackendApplication.java
@@ -2,8 +2,10 @@ package com.zied.authenticationbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AuthenticationBackendApplication {
 
 	public static void main(String[] args) {
