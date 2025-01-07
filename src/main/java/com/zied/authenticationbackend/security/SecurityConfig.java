306c51d4762bc1149459ea0261255501c20e4d42
Zied <zied.kannou15@gmail.com>
2025-01-07 21:00:41 +0100

    Authorized urls UPDATED

--- src/main/java/com/zied/authenticationbackend/security/SecurityConfig.java
+++ src/main/java/com/zied/authenticationbackend/security/SecurityConfig.java
@@ -30,7 +30,15 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(
-                                "/auth/**"
+                                "/auth/**",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html"
                         ).permitAll()
                                 .anyRequest()
                                 .authenticated()
